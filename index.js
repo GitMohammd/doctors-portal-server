87bc80c9bd7f192dfdd3e857cec44fa42cdda9ce
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-03-28 07:31:25 +0600

    give booking data to backend

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 dotenv.config();
 
+
+app.use(express.json())
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z8q3a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -15,11 +17,24 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("doctor's_portal");
+    const appointmentCollection = database.collection("appointments");
+    
+    app.post("/appointment", async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentCollection.insertOne(appointment);
+      console.log(result);
+      res.json(result)
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
