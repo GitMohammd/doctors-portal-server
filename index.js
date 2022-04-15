33c5147ced3e6e328df8b7fd1cba9a5b1e2f382b
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-04-15 11:36:24 +0600

    solving

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(serviceAccount))
+  credential: admin.credential.cert(JSON.parse(serviceAccount).replace(/\\n/g, '\n'))
 });
 
 
